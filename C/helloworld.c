75bd784ba75c139d0bdbd1f14ec979e458171c63
Gr1ll <cyril.kurmann@gmail.com>
2024-06-08 14:44:16 +0200

    C program that does the same as ARM64

--- C/helloworld.c
+++ C/helloworld.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+void print_text(int a, int b);
+
 int main() {
-    printf("Hello, World!\n");
+    int a = 1;
+    int b = 0;
+
+    print_text(a, b);
     return 0;
+}
+
+void print_text(int a, int b) {
+    if(a > b){
+        printf("Hello, World!\n");
+        a = 1;
+        b = 2;
+        print_text(a, b);
+        return;
+    }
+
+    if(a == b){
+        printf("Oh nooo!\n");
+        return;
+    }
+
+    if(a < b){
+        printf("Goodbye, sir!\n");
+        a = 1;
+        b = 1;
+        print_text(a, b);
+        return;
+    }
 }
